cfb22c195ab12e26a2ab5205098e358f87c8fae4
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-05-26 11:03:05 -0500

    Secure S3 bucket configuration

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -14,3 +14,30 @@ resource "aws_s3_bucket" "data_platform_bucket" {
     Project     = "hospitality-hunter"
   }
 }
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.data_platform_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.data_platform_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.data_platform_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
