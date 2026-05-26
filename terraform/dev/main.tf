7eea9e2cb46e59b2603364317eb6bfc5dfb038d2
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-05-26 11:46:48 -0500

    Secure S3 logging bucket

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -69,3 +69,30 @@ resource "aws_s3_bucket_logging" "data_platform_logging" {
   target_bucket = aws_s3_bucket.logs_bucket.id
   target_prefix = "s3-access-logs/"
 }
+resource "aws_s3_bucket_versioning" "logs_versioning" {
+  bucket = aws_s3_bucket.logs_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs_encryption" {
+  bucket = aws_s3_bucket.logs_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "logs_public_access" {
+  bucket = aws_s3_bucket.logs_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
